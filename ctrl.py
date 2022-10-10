af12188015cff719c1630590f0889fb37e61a125
developer1 <dev1@email.com>
2022-10-11 08:47:12 +0900

    Modify pow function using exception

--- ctrl.py
+++ ctrl.py
@@ -1,3 +1,6 @@
+from tkinter import E
+
+
 class Control:
     
     def __init__(self, view):
@@ -40,7 +43,10 @@ class Control:
         return a/b
 
     def pow(self, a, b):
-        if (a==0):
-            return 0
-        else: 
-            return pow(a, b)
+        try:
+            if (a==0):
+                raise Exception("Base Error")
+        except Exception as e:
+            return e
+
+        return pow(a, b)
